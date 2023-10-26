2f43e768b8666aaa8a8c4d3403962a1e4fc593d9
Daniel <ise.daniel@gmail.com>
2023-10-26 18:48:07 -0300

    added struct and function

---
+++ cmd/struct.go
@@ -0,0 +1,6 @@
+package main
+
+type messages struct {
+	error string
+	message string
+}
--- cmd/test-one.go
+++ cmd/test-one.go
@@ -7,13 +7,17 @@ import (
 )
 
 func main() {
+	m := messages{
+		error: "Usage: cmd \"string to be printed\"",
+		message: "I'll print anything i want anyway",
+	}
 	if len(os.Args) != 2 {
 		log.SetFlags(0)
-		log.Fatalln("Usage: cmd \"string to be printed\"")
+		log.Fatalln(m.error)
 	}
-	fmt.Println(Foo())
+	fmt.Println(Foo(m.message))
 }
 
-func Foo() string {
-	return "I'll print anything I want anyway"
+func Foo(s string) string {
+	return s + " (this passed trough Foo function)"
 }
