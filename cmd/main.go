4a7b7147af7ad2abdde191e1b8806092abf481db
Daniel <ise.daniel@gmail.com>
2023-10-31 19:52:07 -0300

    deadlock test

--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"sort"
-)
-
-// "fmt"
-// "log"
-// "os"
-
 func main() {
-	scores := make([]int, 10)
-	for i := 0; i < 10; i++ {
-		scores[i] = int(rand.Intn(100))
-	}
-	sort.Ints(scores)
-
-	worst := make([]int, 5)
-	copy(worst[2:], scores[:1])
-	fmt.Println(worst)
-	fmt.Println(scores)
+	accident()
 }
